fa00632c919128ffa06f8aff19f84065331ee74e
Wouter Meuwis <woutermeuwis@gmail.com>
2025-12-11 17:09:29 +0100

    Day 10 part 1

---
+++ AdventOfCode25/Day 10/Solution.cs
@@ -0,0 +1,89 @@
+namespace AdventOfCode25.Day_10;
+
+public class Solution : BaseSolution
+{
+	protected override void SolveOne(string fileName)
+	{
+		InputReader.ReadAllLines(GetDay(), fileName)
+			.Select(m => new Machine(m))
+			.Sum(m => m.GeShortestCombinationToMatchLights())
+			.Log(Logger, sum => $"The total amount of buttons pressed is {sum}");
+	}
+
+	protected override void SolveTwo(string fileName)
+	{
+		var machines = InputReader.ReadAllLines(GetDay(), fileName)
+			.Select(m => new Machine(m))
+			.ToList();
+		throw new NotImplementedException();
+	}
+}
+
+class Machine
+{
+	private int RequestedIndicatorLights { get; set; }
+	private List<int> Buttons { get; set; } = [];
+	private List<int> Joltages { get; set; } = [];
+
+	public Machine(string input)
+	{
+		foreach (var block in input.Split(' '))
+		{
+			switch (block[0])
+			{
+				case '[':
+					RequestedIndicatorLights = block[1..^1].Select((c, i) => c == '#' ? (int)Math.Pow(2, i) : 0).Sum();
+					break;
+				case '(':
+					Buttons.Add(block[1..^1].Split(',').Select(stringExtensions.ToInt).Select(i => (int)Math.Pow(2, i)).Sum());
+					break;
+				case '{':
+					Joltages.AddRange(block[1..^1].Split(',').Select(stringExtensions.ToInt));
+					break;
+			}
+		}
+	}
+
+	public int GeShortestCombinationToMatchLights()
+	{
+		for (var i = 1; i <= Buttons.Count; i++)
+		{
+			var combinations = Match(Buttons, i);
+			if (combinations.Any(b => b.Aggregate(0, (a, c) => a ^ c) == RequestedIndicatorLights))
+				return i;
+		}
+
+		throw new ("No combination found");
+	}
+	
+	public int GeShortestCombinationToMatchJoltages()
+	{
+		for (var i = 1; i <= Buttons.Count; i++)
+		{
+			var combinations = Match(Buttons, i);
+			if (combinations.Any(b => b.Aggregate(0, (a, c) => a ^ c) == RequestedIndicatorLights))
+				return i;
+		}
+
+		throw new ("No combination found");
+	}
+
+	private List<List<int>> Match(List<int> options, int cnt)
+	{
+		if (cnt == 0) return [];
+
+		var result = new List<List<int>>();
+		foreach (var option in options)
+		{
+			if(cnt == 1)
+				result.Add([option]);
+			else
+			{
+				var cascade = Match(options, cnt - 1);
+				foreach (var c in cascade)
+					result.Add(c.Append(option).ToList());
+			}
+		}
+		return result;
+	}
+}
--- AdventOfCode25/Program.cs
+++ AdventOfCode25/Program.cs
@@ -1,6 +1,6 @@
 ﻿var config = new
 {
-	Day = 9,
+	Day = 10,
 	Days = new[] { 1 },
 	RunMultiple = false,
 	RunExamples = true,
